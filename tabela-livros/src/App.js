f1bbebc9493ef3c02013c3d367fd577440012ca6
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-28 16:24:42 -0300

    Final project

--- tabela-livros/src/App.js
+++ tabela-livros/src/App.js
@@ -5,34 +5,57 @@ import TabelaBody from "./components/TabelaBody";
 import TabelaFoot from "./components/TabelaFoot";
 
 class App extends Component {
+  state = {
+    livros: [],
+  };
 
-state = {
-  livros: [
-    {
-      id: "978-85-7522-632-2",
-      titulo: "CSS Grid Layout",
-      autor: "Mauricio Samy Silva"
-    },
-    {
-      id: "978-85-7522-677-3",
-      titulo: "Node Essencial",
-      autor: "Ricardo R. Lecheta"
-    },
-    {
-      id: "978-85-7522-512-7",
-      titulo: "Aprendendo Material Design",
-      autor: "Kyle Mew"
-    },
-  ]
-}
+  componentDidMount() {
+    fetch("./api/livros.json")
+      .then((response) => response.json())
+      .then((livros) => this.setState({ livros }))
+      .catch(function (error) {
+        console.log("Erro na requisicao");
+      })
+      .finally(function () {
+        console.log("Sempre retorna");
+      });
+  }
 
-  render(){
-  return <table className="tabela">
-    <TabelaHead />
-    <TabelaBody livros={this.state.livros}/>
-    <TabelaFoot />
-  </table>;
-}
+  handleRemoverLinha = (id) => {
+    const livros = this.state.livros.filter((l) => l.id !== id);
+    this.setState({ livros });
+  };
+
+  handleOrdenarCrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    this.setState({ livros });
+  };
+
+  handleOrdenarDecrescente = (titulo) => {
+    const livros = this.state.livros.sort((a, b) =>
+      a.titulo < b.titulo ? -1 : 0
+    );
+    livros.reverse();
+    this.setState({ livros });
+  };
+
+  render() {
+    return (
+      <table className="tabela">
+        <TabelaHead
+          ordenarCrescente={this.handleOrdenarCrescente}
+          ordenarDecrescente={this.handleOrdenarDecrescente}
+        />
+        <TabelaBody
+          livros={this.state.livros}
+          removerLinha={this.handleRemoverLinha}
+        />
+        <TabelaFoot qdeLivros={this.state.livros.length} />
+      </table>
+    );
+  }
 }
 
 export default App;
