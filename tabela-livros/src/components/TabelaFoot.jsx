2c57d3e1de2ea8bab1bbd959c0ca1318c8764917
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-28 11:54:02 -0300

    Created Tabela components

---
+++ tabela-livros/src/components/TabelaFoot.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TabelaFoot = () => (
+    <tfoot>
+        <tr>
+            <td colSpan="4">Este e o rodape da tabela</td>
+        </tr>
+    </tfoot>
+)
+
+export default TabelaFoot;
