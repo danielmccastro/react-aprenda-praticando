f8ca057d1ac6b058cfd12141203b38dd0a54d310
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-30 16:39:01 -0300

    Updated CadastrarLivros component

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,47 +1,57 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 import Menu from "./components/Menu";
 import TabelaLivros from "./components/TabelaLivros";
 import CadastrarLivros from "./components/CadastrarLivros";
 import NotFound from "./components/NotFound";
 
-
 class App extends Component {
-state = {
-  livros: [
-    {
-      id: 1,
-      isbn: "978-85-7522-403-8",
-      titulo: "HTML5 - 2a Edicao",
-      autor: "Mauricio Samy Silva"
-    },
-    {
-      id: 2,
-      isbn: "978-85-7522-807-4",
-      titulo: "Introducao ao Pentest",
-      autor: "Daniel Moreno"
-    },
-    {
-      id: 3,
-      isbn: "978-85-7522-780-8",
-      titulo: "Internet das Coisas para Desenvolvedores",
-      autor: "Ricardo da Silva Ogliari"
-    },
-  ]
-}
+  state = {
+    livros: [
+      {
+        id: 1,
+        isbn: "978-85-7522-403-8",
+        titulo: "HTML5 - 2a Edicao",
+        autor: "Mauricio Samy Silva",
+      },
+      {
+        id: 2,
+        isbn: "978-85-7522-807-4",
+        titulo: "Introducao ao Pentest",
+        autor: "Daniel Moreno",
+      },
+      {
+        id: 3,
+        isbn: "978-85-7522-780-8",
+        titulo: "Internet das Coisas para Desenvolvedores",
+        autor: "Ricardo da Silva Ogliari",
+      },
+    ],
+  };
+
+inserirLivro = livro => {
+  livro.id = this.state.livros.length + 1;
+  this.setState({
+    livros: [...this.state.livros, livro]
+  });
+};
+
   render() {
     return (
-     <Router>
-      <Menu />
-      <Routes>
-        <Route exact path="/" element={<TabelaLivros livros={this.state.livros}/>} />
-        <Route exact path="/cadastrar" element={<CadastrarLivros />} />
-        <Route exact path="*" element={<NotFound />} />
-      </Routes>
-     </Router>
-    )
+      <Router>
+        <Menu />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={<TabelaLivros livros={this.state.livros} />}
+          />
+          <Route exact path="/cadastrar" element={<CadastrarLivros inserirLivro={this.inserirLivro} livro={{id: 0, isbn: "", titulo: "", autor: ""}}/>} />
+          <Route exact path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    );
   }
 }
 
--- crud/src/components/CadastrarLivros.jsx
+++ crud/src/components/CadastrarLivros.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import { redirect } from "react-router-dom";
 
 class CadastrarLivros extends Component {
+  state = {
+    livro: {
+      id: this.props.livro.id,
+      isbn: this.props.livro.isbn,
+      titulo: this.props.livro.titulo,
+      autor: this.props.livro.autor,
+    },
+    redirecionar: false,
+  };
+  handleLivroForm = (e) => {
+    e.preventDefault();
+    this.props.inserirLivro(this.state.livro);
+    this.setState({ redirecionar: true });
+  };
+
   render() {
+    if (this.state.redirecionar === "true") {
+      return redirect("/");
+    }
     return (
       <form>
         <h1>Cadastrar livro</h1>
@@ -13,20 +32,58 @@ class CadastrarLivros extends Component {
           <input
             type="text"
             autoFocus
+            defaultValue={this.props.isbn}
             id="fisbn"
             required
+            value={this.state.livro.isbn}
             pattern="^978-85-7522-[0-9]{3}-[0-9]{1}$"
+            onChange={(e) =>
+              this.setState({
+                livro: {
+                  ...this.state.livro,
+                  isbn: e.target.value,
+                },
+              })
+            }
           />
         </p>
 
         <p>
           <label htmlFor="ftitulo">Titulo</label>
-          <input type="text" id="ftitulo" required />
+          <input
+            type="text"
+            id="ftitulo"
+            ref="titulo"
+            required
+            defaultValue={this.props.titulo}
+            value={this.state.livro.titulo}
+            onChange={(e) =>
+              this.setState({
+                livro: {
+                  ...this.state.livro,
+                  titulo: e.target.value,
+                },
+              })
+            }
+          />
         </p>
-
         <p>
           <label htmlFor="fautor">Autor</label>
-          <input type="text" id="fautor" required />
+          <input
+            type="text"
+            id="fautor"
+            required
+            defaultValue={this.props.autor}
+            value={this.state.livro.autor}
+            onChange={(e) =>
+              this.setState({
+                livro: {
+                  ...this.state.livro,
+                  autor: e.target.value,
+                },
+              })
+            }
+          />
         </p>
         <p>
           <button className="botao cadastrar" type="submit">
