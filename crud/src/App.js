36c8801734d0ec86b1ee3ae6cd8aec9bad914646
Daniel Castro <120190089+danielmccastro@users.noreply.github.com>
2023-06-30 16:11:45 -0300

    Updated components

--- crud/src/App.js
+++ crud/src/App.js
@@ -3,11 +3,34 @@ import Menu from "./components/Menu";
 import TabelaLivros from "./components/TabelaLivros";
 
 class App extends Component {
+state = {
+  livros: [
+    {
+      id: 1,
+      isbn: "978-85-7522-403-8",
+      titulo: "HTML5 - 2a Edicao",
+      autor: "Mauricio Samy Silva"
+    },
+    {
+      id: 2,
+      isbn: "978-85-7522-807-4",
+      titulo: "Introducao ao Pentest",
+      autor: "Daniel Moreno"
+    },
+    {
+      id: 3,
+      isbn: "978-85-7522-780-8",
+      titulo: "Internet das Coisas para Desenvolvedores",
+      autor: "Ricardo da Silva Ogliari"
+    },
+  ]
+}
+
   render() {
     return (
       <div className="App">
         <Menu />
-        <TabelaLivros />
+        <TabelaLivros livros={this.state.livros}/>
       </div>
     )
   }
--- crud/src/components/TabelaLivros.jsx
+++ crud/src/components/TabelaLivros.jsx
@@ -1,33 +1,38 @@
 import React from "react";
 
-const TabelaLivros = (props) => {
+const TabelaLivros = ({ livros }) => {
   return (
     <div className="livros">
       <h1>Tabela livros</h1>
-      <table className="tabela">
-        <thead>
-          <tr>
-            <th width="17%">ISBN</th>
-            <th>Titulo</th>
-            <th>Autor</th>
-            <th width="7%"></th>
-            <th width="9%"></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>ISBN do livro</td>
-            <td>Titulo do livro</td>
-            <td>Autor do livro</td>
-            <td>
-              <button className="botao editar">Editar</button>
-            </td>
-            <td>
-              <button className="botao remover">Remover</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      {livros.length === 0 && <h2>Nenhum livro cadastrado!</h2>}
+      {livros.length > 0 && (
+        <table className="tabela">
+          <thead>
+            <tr>
+              <th width="17%">ISBN</th>
+              <th>Titulo</th>
+              <th>Autor</th>
+              <th width="7%"></th>
+              <th width="9%"></th>
+            </tr>
+          </thead>
+          <tbody>
+            {livros.map((livro) => (
+              <tr key={livro.isbn}>
+                <td>{livro.isbn}</td>
+                <td>{livro.titulo}</td>
+                <td>{livro.autor}</td>
+                <td>
+                  <button className="botao editar">Editar</button>
+                </td>
+                <td>
+                  <button className="botao remover">Remover</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
